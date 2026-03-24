c9ddea0e3be0dd9a7e10fa2566529ffd3ee9d934
mwiraszka <michal@wiraszka.com>
2026-03-24 22:59:54 +0100

    feat: link preview badge to PR and add secondary color for labels

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   clerkPublishableKey: 'pk_live_Y2xlcmsuYnJld3NraWJldHMuY29tJA',
   apiUrl: 'https://api.brewskibets.com/api',
-  preview: null as { branch: string; commit: string; date: string } | null,
+  preview: null as { branch: string; commit: string; date: string; prUrl: string } | null,
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   clerkPublishableKey: 'pk_test_ZGFzaGluZy1nb2xkZmlzaC0zMy5jbGVyay5hY2NvdW50cy5kZXYk',
   apiUrl: 'http://localhost:3000/api',
-  preview: null as { branch: string; commit: string; date: string } | null,
+  preview: null as { branch: string; commit: string; date: string; prUrl: string } | null,
 };
